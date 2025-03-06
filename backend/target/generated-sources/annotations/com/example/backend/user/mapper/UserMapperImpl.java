e692ef97bcfce084928038b4f753a30daaa484b7
timothy-lew <timothyjblew2000@gmail.com>
2025-03-06 18:34:54 +0800

    add mock response from backend ui

--- backend/target/generated-sources/annotations/com/example/backend/user/mapper/UserMapperImpl.java
+++ backend/target/generated-sources/annotations/com/example/backend/user/mapper/UserMapperImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-03-06T14:17:50+0800",
-    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 23.0.1 (Amazon.com Inc.)"
+    date = "2025-03-06T16:52:30+0800",
+    comments = "version: 1.5.2.Final, compiler: Eclipse JDT (IDE) 3.41.0.z20250213-2037, environment: Java 23.0.1 (Amazon.com Inc.)"
 )
 @Component
 public class UserMapperImpl implements UserMapper {
